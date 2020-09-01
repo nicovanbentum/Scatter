167962fae41d5332b1b644d846bf7155224583bc
Nico van Bentum <niico0708@gmail.com>
2020-09-01 12:20:35 +0200

    added pch and removed x86

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hallo Nico, je zus! \n";
-}
---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main() {
+    std::puts("Hello World");
+    std::cin.get();
+}
