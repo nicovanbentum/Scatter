f5298ad7e659fc03e0fc646bce35a525e30fdf53
Nico van Bentum <niico0708@gmail.com>
2020-10-07 12:56:58 +0200

    removed application

--- header/Application.h
+++ header/Application.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "pch.h"
+
+#include "Device.h"
+#include "ShaderManager.h"
+#include "CommandBufferManager.h"
+#include "PipelineManager.h"
+
+namespace scatter {
+
+    class VulkanApplication {
+    public:
+        VulkanApplication();
+        ~VulkanApplication();
+
+        void update(float dt);
+
+        VkPipeline createPipeline();
+
+    private:
+        VulkanDevice device;
+        VulkanShaderManager shaderManager;
+        CommandBufferManager commandBufferManager;
+        VulkanPipelineManager pipelineManager;
+
+        VkDebugUtilsMessengerEXT debugMessenger;
+
+        GLFWwindow* window;
+        VkSurfaceKHR surface;
+        VkSemaphore semaphore;
+        VkFence fence;
+
+    };
+
+}
