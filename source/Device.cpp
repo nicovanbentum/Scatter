f3c9ebcecaebea8c6883f0e3c9f55f67ce2ee16e
Nico van Bentum <niico0708@gmail.com>
2021-01-22 17:03:20 +0100

    command pool reset flag

--- source/Device.cpp
+++ source/Device.cpp
@@ -83,7 +83,7 @@ void VulkanDevice::createCommandPool() {
     VkCommandPoolCreateInfo poolInfo{};
     poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
     poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
-    poolInfo.flags = 0;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
 
     if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
         throw std::runtime_error("failed to create command pool! \n");
